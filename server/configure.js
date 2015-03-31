95f87c5f38cb9557fe35cec8461be0a9eb355d78
Vaibhav Pandey <vaibhav.p@tyroo.com>
2015-03-31 22:25:20 +0530

    app migrated to express 4.0

--- server/configure.js
+++ server/configure.js
@@ -1,7 +1,13 @@
-var connect = require('connect'),
-	path = require('path'),
+// var connect = require('connect'),
+var path = require('path'),
 	routes = require('./routes'),
-	exphbs = require('express3-handlebars');
+	exphbs = require('express3-handlebars'),
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
+	morgan = require('morgan'),
+	methodOverride = require('method-override'),
+	errorHandler = require('errorHandler');
 
 module.exports = function(app){
 	//configuration code...
@@ -20,21 +26,32 @@ module.exports = function(app){
 
 	// configure various middlewares part of connect. 
 
-	app.use(connect.logger('dev')); 
-	app.use(connect.bodyParser({
-		uploadDir:path.join(__dirname, '../public/upload/temp')
+	// app.use(connect.logger('dev')); 
+	app.use(morgan('dev'));
+	// app.use(connect.bodyParser({
+	// 	uploadDir:path.join(__dirname, '../public/upload/temp')
 
+	// }));
+	// app.use(connect.json());
+	// app.use(connect.urlencoded());
+	
+	app.use(bodyParser({
+		uploadDir:path.join(__dirname,'../public/upload/temp')
 	}));
-	app.use(connect.json());
-	app.use(connect.urlencoded());
-	app.use(connect.methodOverride());
-	app.use(connect.cookieParser('who-was-the-hero-of-matrix'));
-	app.use(app.router);
-	app.use('/public', connect.static(path.join(__dirname, '../public')));
+	// app.use(connect.methodOverride());
+	app.use(methodOverride());
+
+	// app.use(connect.cookieParser('who-was-the-hero-of-matrix'));
+	app.use(cookieParser('who-was-the-hero-of-matrix'));
+	// app.use(app.router);
+	routes.initialize(app, new express.Router());
+	// app.use('/public/', connect.static(path.join(__dirname, '../public')));
+	app.use('/public/', express.static(path.join(__dirname,'../public')));
 
 	if('development' === app.get('env')) {
-		app.use(connect.errorHandler());
+		// app.use(connect.errorHandler());
+		app.use(errorHandler());
 	}
-	routes.initialize(app);
+	
 	return app;
 };
--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,20 @@
 var home = require('../controllers/home'),
 	image = require('../controllers/image');
 
-module.exports.initialize = function(app){
-	app.get('/', home.index);
-	app.get('/images/:image_id', image.index);
+module.exports.initialize = function(app, router){
+	// app.get('/', home.index);
+	router.get('/', home.index);
+	// app.get('/images/:image_id', image.index);
+	router.get('/images/:image_id', image.index);
 
-	app.post('/images', image.create);
-	app.post('/images/:image_id/like', image.like);
-	app.post('/images/:image_id/comment', image.comment);
+	// app.post('/images', image.create);
+	router.post('/images', image.create);
+	// app.post('/images/:image_id/like', image.like);
+	router.post('/images/:image_id/like', image.like);
+
+	// app.post('/images/:image_id/comment', image.comment);
+	router.post('/images/:image_id/comment', image.comment);
+
+	app.use('/',router);
 
 } ;
